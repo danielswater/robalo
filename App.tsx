8eb2238d2b4d4003df9ce7e067330a00c7c8d7d8
Daniel Swater <danielswater@gmail.com>
2025-12-14 10:07:13 -0300

    Projeto inicial do app Comanda Digital

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Comanda Digital</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 24,
+  },
+});
